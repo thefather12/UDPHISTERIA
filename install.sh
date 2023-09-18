bbe6abc09f51650a9d6eb7f57cc39260ebe60c10
thefather12 <94747184+thefather12@users.noreply.github.com>
2023-09-17 23:47:19 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -40,11 +40,11 @@ systemctl daemon-reload
 systemctl start udpmod
 systemctl enable udpmod
 
-echo " obfs: ${OBFS}" > ${dir}/UDPMOD/data
+echo " OBFS: ${OBFS}" > ${dir}/UDPMOD/data
 echo "PUERTOS: 36712" >> ${dir}/UDPMOD/data
 echo "RANGO DE PUERTOS: 10000:65000" >> ${dir}/UDPMOD/data
 cat ${dir}/UDPMOD/data
- echo -ne "\n\033[1;31mCOPIE LOS DATOS Y PRECIONE ENTER \033[1;33mpara volver al \033[1;32mMENU!\033[0m"; read
+ echo -ne "\n\033[1;31mCOPIE LOS DATOS Y PRECIONE ENTER \033[1;33mPARA VOLVER AL \033[1;32mMENU!\033[0m"; read
    menu
 
    fi
